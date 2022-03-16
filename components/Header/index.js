6604bcb8be3c133987ec7903bddcd33ebd0dbb87
jay-0331 <jayvekariya75@gmail.com>
2022-03-16 16:06:39 +0530

    commit

--- components/Header/index.js
+++ components/Header/index.js
@@ -15,7 +15,7 @@ const Header = () => {
         <nav className="flex w-full items-center justify-between">
             <p className="text-lg md:text-2xl font-extrabold">Where In The World?</p>
             <button className="w-12 h-12 shadow-light-neumorphism-xl dark:shadow-dark-neumorphism-xl rounded-lg flex justify-center items-center hover:shadow-light-neumorphism-l active:shadow-light-inner-neumorphism-l dark:hover:shadow-dark-neumorphism-l dark:active:shadow-dark-inner-neumorphism-l" onClick={modeChange}>
-              {isDarkMode ? <HiMoon size={'2rem'} className="fill-current"/> : <HiSun size={'2rem'} className="fill-current"/>}
+              {isDarkMode ? <HiMoon role={"article"} size={'2rem'} className="fill-current"/> : <HiSun role={"article"} size={'2rem'} className="fill-current"/>}
             </button>
         </nav>
     </header>
